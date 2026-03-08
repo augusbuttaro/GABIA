4b5f46098582693bdad75de246d8403e490b8d11
Augusto Buttaro <augusbuttaro@gmail.com>
2026-03-08 20:38:45 -0300

    prod

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import DiagonalBackground from "../components/DiagonalBackground";
 
 const Layout = () => {
   return (
