cfc26895bfaf65564f37f06b927f4423b9e521a8
Augusto Buttaro <augusbuttaro@gmail.com>
2026-03-08 20:42:23 -0300

    update fetch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
         }
 
         try {
-            const res = await fetch('/api/contact', {
+            const res = await fetch('https://gabia-9dzk.onrender.com/api/contact', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
