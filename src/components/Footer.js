56667748e23d7e6ad8b72a4438506059a9116ee1
Augusto Buttaro <augusbuttaro@gmail.com>
2026-03-08 20:24:59 -0300

    Prod

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,29 @@
-import { FaInstagram, FaRegEnvelope, FaWhatsapp, FaMapLocationDot } from "react-icons/fa6";
+import { FaInstagram, FaRegEnvelope, FaWhatsapp } from "react-icons/fa6";
 
 const Footer = () => {
     return(
         <div>
             <footer className="flex bg-gray-800 text-white py-8 px-4 justify-around items center">
-                <div className="flex flex-col justify-center items-end">
-                    <div className="flex items-center gap-2">
-                        <FaMapLocationDot className="text-xl hover:text-gray-400 transition-colors"/>
-                        <p>Siempreviva 123</p>
-                    </div>
-                    <p>Springfield, CA</p>
-                </div>
                 <div className="text-center flex items-center">
                     <p className="text-sm">© 2025 GABIA. Todos los derechos reservados.</p>
                 </div>
                 <div>
                     <div>
-                        <a className="flex items-center gap-2" href="https://www.instagram.com/instagram" target="_blank" rel="noopener noreferrer">
+                        <a className="flex items-center gap-2" target="_blank" rel="noopener noreferrer">
                             <FaInstagram className="text-xl hover:text-gray-400 transition-colors" />
-                            <p>gabia.shyma</p>
+                            <p>gabia.seh</p>
                         </a>
                     </div>
                     <div>
-                        <a className="flex items-center gap-2" href="https://www.instagram.com/instagram" target="_blank" rel="noopener noreferrer">
+                        <a className="flex items-center gap-2" href="mailto:gabiaseh@gmail.com?subject=Consulta%20desde%20el%20sitio%20web" target="_blank" rel="noopener noreferrer">
                             <FaRegEnvelope className="text-xl hover:text-gray-400 transition-colors" />
-                            <p>contacto@gabia.com.ar</p>
+                            <p>gabiaseh@gmail.com</p>
                         </a>
                     </div>
                     <div>
-                        <a className="flex items-center gap-2" href="https://www.instagram.com/instagram" target="_blank" rel="noopener noreferrer">
+                        <a className="flex items-center gap-2" href="https://wa.me/5492364547389?text=Hola,%20me%20gustaría%20contactarme%20con%20ustedes" target="_blank" rel="noopener noreferrer">
                             <FaWhatsapp className="text-xl hover:text-gray-400 transition-colors" />
-                            <p>+54 9 2364 123456</p>
+                            <p>+54 9 2364 547389</p>
                         </a>
                     </div>
                 </div>
